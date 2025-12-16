47ddd5e472a385e6c37de34d1872631e61f8841e
kwon <kyjyj6334@gmail.com>
2025-12-16 17:12:06 +0900

    Initial commit: Spring Boot + React structure

---
+++ src/main/java/com/example/starbucks_react/StarbucksReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.starbucks_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarbucksReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarbucksReactApplication.class, args);
+	}
+
+}
