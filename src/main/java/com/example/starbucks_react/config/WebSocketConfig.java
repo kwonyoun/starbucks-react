41e6aff4726ee6e87bb11e8ab26b54c6ccdb5152
kwon <kyjyj6334@gmail.com>
2025-12-21 15:38:46 +0900

    feat: add websocket

---
+++ src/main/java/com/example/starbucks_react/config/WebSocketConfig.java
@@ -0,0 +1,28 @@
+package com.example.starbucks_react.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    // 엔드포인트 등록을 위한 설정
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("http://localhost:3000")
+                .withSockJS();
+    }
+
+    // prefix로 sub이 붙으면 구독, pub이 붙으면 메시지 송신
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/pub");
+    }
+    
+}
--- src/main/java/com/example/starbucks_react/controller/OrderController.java
+++ src/main/java/com/example/starbucks_react/controller/OrderController.java
@@ -4,29 +4,47 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.starbucks_react.dto.OrderStatusMessage;
 import com.example.starbucks_react.dto.StoreDto;
 import com.example.starbucks_react.service.OrderService;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/stores")
+@RequestMapping("/api/orders")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:3000")
 public class OrderController {
 
     private final  OrderService orderService;
+    private final ChatController chatController;
 
-    @GetMapping("/near")
-    public List<StoreDto> getNearestStores(
-        @RequestParam double lat,
-        @RequestParam double lng
-    ) {
-        return orderService.getNearestStores(lat, lng);
+    // @GetMapping("/near")
+    // public List<StoreDto> getNearestStores(
+    //     @RequestParam double lat,
+    //     @RequestParam double lng
+    // ) {
+    //     return orderService.getNearestStores(lat, lng);
+    // }
+
+    @PostMapping("/{id}/status")
+    public void updateStatus(@PathVariable int id,
+                            @RequestParam String status) {
+
+        orderService.updateStatus(id, status);
+
+        OrderStatusMessage msg = new OrderStatusMessage();
+        msg.setOrderId(id);
+        msg.setStatus(status);
+
+        chatController.sendOrderStatus(msg);
     }
+
     
 }
